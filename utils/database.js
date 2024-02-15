00ba6c0f6b6a9a30cf2b817f3fb513a336fe2121
PC <rahioui.youssef04@gmail.com>
2024-02-15 10:13:50 +0100

    eight commit

--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   console.log("process");
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI.toString(), {
     });
     console.log('MongoDB connected successfully');
   } catch (error) {
